a41edbfe75561887910b7b1dd51fc184c07a204f
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-09-09 16:28:39 +0530

    Fixed logo

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
 
             <nav class="navbar navbar-expand-lg bg-white border-bottom">
                 <div class="container">
-                    <Link class="navbar-brand" to="/"> <img src="/media/logooo.PNG"></img></Link>
+                    <Link class="navbar-brand" to="/"> <img src="/media/logooo.PNG" style={{ height: "40px", width: "auto" }}></img></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
