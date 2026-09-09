8ceb739806d2b1c5bc237106b65bc244b87515e6
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-09-09 16:22:37 +0530

    Changed stlying

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
-    return ( 
-        <div className='container p-4'>
+    const navigate = useNavigate();
+
+    return (
+        <div className="container p-4">
             <div className="row text-center">
 
-                <img src="https://zerodha.com/static/images/landing.svg" alt="hero image" className='mb-5'></img>
+                <img
+                    src="https://zerodha.com/static/images/landing.svg"
+                    alt="hero image"
+                    className="mb-5"
+                />
 
+                <h1 className="mt-5">Invest in everything</h1>
 
-                <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button style={{width:"25%",margin:"0 auto"}} type="button" className="btn btn-primary p-3 mb-5">Sign up for free</button>
+                <p>
+                    Online platform to invest in stocks, derivatives, mutual funds,
+                    ETFs, bonds, and more.
+                </p>
+
+                <button
+                    style={{ width: "25%", margin: "0 auto" }}
+                    type="button"
+                    className="btn btn-primary p-3 mb-5"
+                    onClick={() => navigate("/signup")}
+                >
+                    Sign up for free
+                </button>
+
             </div>
         </div>
-     );
+    );
 }
 
 export default Hero;
